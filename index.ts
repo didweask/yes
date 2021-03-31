740f71789c4dd3ed2dac13d905fe976088ae12fe
Tika <48658947+tika@users.noreply.github.com>
2021-03-31 12:29:31 +0100

    did we ask?
    no, we did not.

--- index.ts
+++ index.ts
@@ -1,7 +1,154 @@
-export * from 'import from * export as const';
+export * from 'when';
 
-function const removeEverythign = () => {
-  globalThis = null;
+function const printIfWeAsked = (isUwu: bean) => {
+  // make sure EVERY language knows if we asked
+  // 1. Backbone.js
+	// TODO
+	
+  // 2. Bash
+  echo isUwu ? "no, we did not" : "no, we did not"
+
+
+  // 3. Basic
+  PRINT isUwu ? "no, we did not" : "no, we did not"​
+
+
+  // 4. C
+  #include 
+
+  int main(void)
+  {
+      puts(isUwu ? "no, we did not" : "no, we did not");
+  }
+
+
+  // 5. C++
+  #include 
+
+  int main()
+  {
+      std::cout << isUwu ? "no, we did not" : "no, we did not";
+      return 0;
+  }
+
+
+  // 6. C#
+  using System;
+  class Program
+  {
+      public static void Main(string[] args)
+      {
+          Console.WriteLine(isUwu ? "no, we did not" : "no, we did not");
+      }
+  }
+
+
+  // 7. Clipper
+  ? isUwu ? "no, we did not" : "no, we did not"
+
+
+  // 8. CoffeeScript
+  console.log isUwu ? "no, we did not" : "no, we did not"
+
+
+  // 9. Delphi
+  program HelloWorld;
+  begin
+    Writeln(isUwu ? "no, we did not" : "no, we did not");
+  end.
+
+
+  // 10. HTML
+
+   Hello World!
+
+  // 11. Java
+  import javax.swing.JFrame;  //Importing class JFrame
+  import javax.swing.JLabel;  //Importing class JLabel
+  public class DidWeAskMainFrameIntoMatrixPublicVoidClassExtendsMinorChangeWithYesIndexTSWhenDiDIAskTypeBeat {
+      public static void main(String[] args) {
+          JFrame frame = new JFrame();           //Creating frame
+          frame.setTitle("Did we ask");                 //Setting title frame
+          frame.add(new JLabel(isUwu ? "no, we did not" : "no, we did not"));//Adding text to frame
+          frame.pack();                          //Setting size to smallest
+          frame.setLocationRelativeTo(null);     //Centering frame
+          frame.setVisible(true);                //Showing frame
+      }
+  }
+
+
+  // 12. JavaScript
+  document.write(isUwu ? "no, we did not" : "no, we did not");
+
+
+  // 13. jQuery
+  $("body").append(isUwu ? "no, we did not" : "no, we did not");
+
+
+  // 14. Julia
+  println(isUwu ? "no, we did not" : "no, we did not")
+
+
+  // 15. Logo
+  print [isUwu ? "no, we did not" : "no, we did not"]
+
+
+  // 16. MatLab
+  disp(isUwu ? "no, we did not" : "no, we did not")
+
+
+  // 17. Objective-C
+  #import 
+  #import 
+
+  int main(void)
+  {
+      NSLog(@isUwu ? "no, we did not" : "no, we did not");
+      return 0;
+  }
+
+
+  // 18. Pascal
+  program HelloWorld;
+  begin
+    WriteLn(isUwu ? "no, we did not" : "no, we did not");
+  end.
+
+
+  // 19. Perl 5
+  print isUwu ? "no, we did not" : "no, we did not";
+
+
+  // 20. PHP
+  // TODO
+
+  // 21. Processing
+  void setup(){
+    println(isUwu ? "no, we did not" : "no, we did not");
+  }
+
+  // 22. Python
+  print isUwu ? "no, we did not" : "no, we did not"
+
+  // 23. R
+  cat('Hello, world!
+  ')
+
+  // 24. Ruby
+  puts isUwu ? "no, we did not" : "no, we did not"
+
+  // 25. Swift
+  println(isUwu ? "no, we did not" : "no, we did not")
+
+  // 26. VBScript
+  MsgBox isUwu ? "no, we did not" : "no, we did not"
+
+  // 27. Visual Basic .NET
+  Module Module1
+      Sub Main()
+          Console.WriteLine(isUwu ? "no, we did not" : "no, we did not")
+      End Sub
+  End Module
 }
 
-removeEverythign();
+printIfWeAsked("false");
